830507d1143ec76a97e2af78393cb5faa9a056b1
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2012-02-17 14:30:49 +0600

    Show notification that non ASCII symbols can`t be used in login.

--- src/ru/redsolution/bst/ui/dialog/AuthorizationDialogBuilder.java
+++ src/ru/redsolution/bst/ui/dialog/AuthorizationDialogBuilder.java
@@ -8,9 +8,12 @@ import ru.redsolution.dialogs.DialogBuilder;
 import android.app.Activity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Диалог авторизации.
+ * 
+ * Диалог не может быть закрыт кнопкой назад.
  * 
  * Автоматически сохраняет введенные имя пользователя и пароль.
  * 
@@ -22,16 +25,26 @@ public class AuthorizationDialogBuilder extends ConfirmDialogBuilder {
 	private final EditText loginView;
 	private final EditText passwordView;
 
-	public AuthorizationDialogBuilder(Activity activity, int dialogId,
+	public AuthorizationDialogBuilder(final Activity activity, int dialogId,
 			final AcceptAndDeclineDialogListener listener) {
 		super(activity, dialogId, new AcceptAndDeclineDialogListener() {
 
 			@Override
 			public void onAccept(DialogBuilder dialogBuilder) {
-				BST.getInstance().setLoginAndPassword(
-						((AuthorizationDialogBuilder) dialogBuilder).getLogin(),
-						((AuthorizationDialogBuilder) dialogBuilder).getPassword());
-				listener.onAccept(dialogBuilder);
+				String login = ((AuthorizationDialogBuilder) dialogBuilder)
+						.getLogin();
+				if (!login.matches("^[\\u0000-\\u007F]*$")) {
+					Toast.makeText(activity,
+							activity.getString(R.string.incorrect_login),
+							Toast.LENGTH_LONG).show();
+					listener.onDecline(dialogBuilder);
+				} else {
+					BST.getInstance().setLoginAndPassword(
+							login,
+							((AuthorizationDialogBuilder) dialogBuilder)
+									.getPassword());
+					listener.onAccept(dialogBuilder);
+				}
 			}
 
 			@Override
@@ -58,4 +71,5 @@ public class AuthorizationDialogBuilder extends ConfirmDialogBuilder {
 	public String getPassword() {
 		return passwordView.getText().toString();
 	}
+
 }

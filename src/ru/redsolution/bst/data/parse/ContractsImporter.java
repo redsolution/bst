27ed3db3fdbddfd9b9e18578b29a29c21f9ae81b
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2013-08-03 16:55:27 +0600

    Add license and readme.

--- src/ru/redsolution/bst/data/parse/ContractsImporter.java
+++ src/ru/redsolution/bst/data/parse/ContractsImporter.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2013, Redsolution LTD. All rights reserved.
+ *
+ * This file is part of Barcode Scanner Terminal project;
+ * you can redistribute it and/or modify it under the terms of
+ *
+ * Barcode Scanner Terminal is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 package ru.redsolution.bst.data.parse;
 
 public class ContractsImporter extends ContainerImporter {
